3bea422443362deff9b3257fb956a3b78f4e8052
anna-1804@mail.ru <anna-1804@mail.ru>
2016-06-29 00:30:39 +0300

    Реализован Navigation Drawer Реализована обработка нажатия системной кнопки back (ожидаемый  результат закрыть NavigationDrawerесли он развернут)  Реализовано программное скруглениефото аватарапользователя   (Navigation Drawer)    Реализован Collapsing Toolbar     Реализовать сворачивание серой плашки с значениями      пользователя при скролле до минимально возможного,используя      Layout Behavior (решение сырое)

---
+++ app/src/main/java/com/softdesign/devintensive/util/CollapsingBehavior.java
@@ -0,0 +1,52 @@
+package com.softdesign.devintensive.util;
+
+
+import android.content.Context;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.NestedScrollView;
+import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class CollapsingBehavior extends CoordinatorLayout.Behavior<LinearLayout> {
+
+    private Context mContext;
+
+    public CollapsingBehavior(){
+
+    }
+
+    public CollapsingBehavior(Context context, AttributeSet attributeSet){
+        super(context,attributeSet);
+        mContext = context;
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, LinearLayout child, View dependency) {
+        return  dependency instanceof NestedScrollView;
+    }
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, LinearLayout child, View dependency) {
+        int tempHeight = child.getHeight();
+        float depY = dependency.getY()-228;
+
+        if (depY >= 128 && depY <=224){
+            tempHeight = (int)(depY/3);
+        }
+        dependency.setPadding(dependency.getPaddingLeft(),tempHeight,dependency.getPaddingRight(),dependency.getPaddingBottom());
+        child.setY(dependency.getY());
+        if (depY <= 128) {
+            child.setPadding(child.getPaddingLeft(), 0, child.getPaddingRight(), 0);
+        } else {
+            child.setPadding(child.getPaddingLeft(), 56, child.getPaddingRight(), 56);
+        }
+
+        return true;
+    }
+
+}
